e3dede891d1a5805c619fc1fcb6b811039079b8e
Juan G Lopez <nahujg@gmail.com>
2022-07-11 17:09:40 +0200

    iteración 9

--- src/App.js
+++ src/App.js
@@ -38,8 +38,11 @@ function App() {
   return (
     <div className="App">
       <Tweet tweet={ tweetsArray[0] }/>
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
     </div>
   );
 }
+
 
 export default App;
